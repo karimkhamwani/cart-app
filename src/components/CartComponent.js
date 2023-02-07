df944ed11b960bc240695e276a56b6d5cc6ee918
karimkhamwani <karim.khamwani@hotmail.com>
2023-02-07 11:47:39 -0500

    checkbox background updated

--- src/components/CartComponent.js
+++ src/components/CartComponent.js
@@ -2,34 +2,10 @@ import { memo, useMemo, useState } from "react";
 import CartList from "./CartList";
 import Card from "react-bootstrap/Card";
 import Checkout from "./Checkout";
+import { products as p } from "../util";
 
 const CartComponent = memo(() => {
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: "Watch",
-      image:
-        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-      quantity: 0,
-      price: 10,
-    },
-    {
-      id: 2,
-      name: "Product 1",
-      image:
-        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-      quantity: 0,
-      price: 20,
-    },
-    {
-      id: 3,
-      name: "Product 1",
-      image:
-        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-      quantity: 0,
-      price: 30,
-    },
-  ]);
+  const [products, setProducts] = useState(p);
 
   const amount = useMemo(
     () => products.reduce((pre, curr) => pre + curr.quantity * curr.price, 0),
---
+++ src/util.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Watch",
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+    quantity: 0,
+    price: 10,
+  },
+  {
+    id: 2,
+    name: "Product 1",
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+    quantity: 0,
+    price: 20,
+  },
+  {
+    id: 3,
+    name: "Product 1",
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+    quantity: 0,
+    price: 30,
+  },
+];
